b5f5251d4807fa0573236f5901702ebf7c26d7d1
Sriharsha Tikkireddy <sri.tikkireddy@databricks.com>
2021-04-06 16:14:18 -0400

    refactor debugexecution into terraform execution and then add a option to submit debug commands where it wont run the actual workflow
    Signed-off-by: Sriharsha Tikkireddy <sri.tikkireddy@databricks.com>

--- databricks_sync/cmds/triage.py
+++ databricks_sync/cmds/triage.py
@@ -10,7 +10,7 @@ from databricks_sync.cmds.config import git_url_option, ssh_key_option, inject_p
     local_git_option, validate_git_params, handle_additional_debug, \
     wrap_with_user_agent
 from databricks_sync.sdk.sync.constants import GeneratorCatalog
-from databricks_sync.sdk.sync.import_ import DebugExecution
+from databricks_sync.sdk.sync.import_ import TerraformExecution
 
 SUPPORTED_IMPORTS = [
     GeneratorCatalog.CLUSTER_POLICY,
@@ -53,9 +53,9 @@ def triage_cli(ctx, git_ssh_url, local_git_path, databricks_object_type, backend
     handle_additional_debug(ctx)
     validate_git_params(git_ssh_url, local_git_path)
     back_end_json = Path(backend_file) if backend_file is not None else None
-    te = DebugExecution(folders=databricks_object_type, refresh=False, revision=revision, plan=False,
-                        plan_location=Path("/tmp/test") / "plan.out",
-                        local_state_location=Path("/tmp/test") / "state.tfstate", apply=False, destroy=False,
-                        git_ssh_url=git_ssh_url, local_git_path=local_git_path, api_client=api_client,
-                        branch=branch, post_import_shutdown=True, back_end_json=back_end_json)
-    te.execute(commands=command)
+    te = TerraformExecution(folders=databricks_object_type, refresh=False, revision=revision, plan=False,
+                            plan_location=Path("/tmp/test") / "plan.out",
+                            local_state_location=Path("/tmp/test") / "state.tfstate", apply=False, destroy=False,
+                            git_ssh_url=git_ssh_url, local_git_path=local_git_path, api_client=api_client,
+                            branch=branch, post_import_shutdown=True, back_end_json=back_end_json)
+    te.execute(debug_commands=command)
--- databricks_sync/sdk/sync/import_.py
+++ databricks_sync/sdk/sync/import_.py
@@ -4,7 +4,7 @@ import os
 import shutil
 import tempfile
 from pathlib import Path
-from typing import List
+from typing import List, Optional
 
 from databricks_cli.clusters.api import ClusterApi
 from databricks_cli.sdk import ApiClient
@@ -113,7 +113,7 @@ class TerraformExecution:
 
     @setup_empty_stage
     @setup_repo
-    def execute(self, stage_path, repo_path):
+    def execute(self, stage_path, repo_path, debug_commands: Optional[List[str]] = None):
         # TODO test for each action, stop and retrun failure as needed
         # setup provider and init
         stage_path.mkdir(parents=True)
@@ -141,6 +141,13 @@ class TerraformExecution:
             # TODO: Edge case incase the user switches to another admin user, there is a chance the prior user will be scimmed
             # in and managed. So if the current user is in state and we run destroy we should run terraform state rm curr_user
             pass
+
+        # Debug mode
+        if debug_commands is not None and len(debug_commands) > 0:
+            for command in debug_commands:
+                tf.raw_cmd(command)
+            # Exit when debug commands are finished running
+            return
 
         # We should run validate in either case
         tf.validate()
@@ -164,82 +171,6 @@ class TerraformExecution:
                 if out is not None:
                     for cluster_id in fetch_cluster_ids_from_state(out):
                         shutdown_clusters(self.api_client, cluster_id)
-
-
-class DebugExecution:
-    def __init__(self, folders: List[str], refresh: bool = True, revision: str = None, plan: bool = False,
-                 plan_location: Path = None, local_state_location: Path = None, apply: bool = False,
-                 destroy: bool = False,
-                 git_ssh_url: str = None, local_git_path=None, api_client: ApiClient = None, branch="master",
-                 post_import_shutdown=False, back_end_json: Path = None):
-
-        self.back_end_json: Path = back_end_json
-        self.post_import_shutdown = post_import_shutdown
-        self.tmp_stage = tempfile.TemporaryDirectory(prefix="databricks_sync")
-        self.local_git_path = local_git_path
-        self.revision = revision
-        self.branch = branch
-        self.folders = folders
-        self.git_ssh_url = git_ssh_url
-        self.local_state_location = local_state_location
-        self.destroy = destroy
-        self.refresh = refresh
-        self.apply = apply
-        self.plan_location = plan_location
-        self.plan = plan
-        self.api_client = api_client
-
-    def __del__(self):
-        self.tmp_stage.cleanup()
-
-    def validate(self):
-        if self.apply and self.destroy:
-            raise ValueError("apply and destroy cannot be run at the same time")
-
-    @staticmethod
-    def __get_exports_path(base_path) -> Path:
-        return base_path / "exports"
-
-    def __stage_all_json_files(self, stage_path, repo_path):
-        istg = ImportStage(stage_path)
-        for folder in self.folders:
-            istg.stage_files(self.__get_exports_path(repo_path) / folder)
-        istg.stage_file(self.__get_exports_path(repo_path) / "mapped_variables.tf.json")
-
-    @setup_empty_stage
-    @setup_repo
-    def execute(self, stage_path, repo_path, commands):
-        # TODO test for each action, stop and retrun failure as needed
-        # setup provider and init
-        stage_path.mkdir(parents=True)
-        with (stage_path / "main.tf.json").open("w+") as w:
-            resp = MeConstants.set_me_variable(ENTRYPOINT_MAIN_TF,
-                                               get_me_username(self.api_client))
-            if "provider" in resp and "databricks" in resp.get("provider", {}):
-                resp["provider"]["databricks"]["host"] = os.getenv("DATABRICKS_HOST",
-                                                                   "unable to find host - DB Sync Profile Required")
-            main_tf_file_content = json.dumps(resp,
-                                              indent=4, sort_keys=True)
-            log.info("Main TF File: " + main_tf_file_content)
-            w.write(main_tf_file_content)
-            if self.back_end_json is not None:
-                log.info("Backend Json TF File: " + str(self.back_end_json))
-                shutil.copy(self.back_end_json, stage_path / "backend.tf.json")
-
-        tf = Terraform(working_dir=str(stage_path), is_env_vars_included=True)
-        tf.version()
-        tf.init()
-        # if we are not destroying download the git repo and stage tf files
-        if self.destroy is False:
-            self.__stage_all_json_files(stage_path, repo_path)
-        else:
-            # TODO: Edge case incase the user switches to another admin user, there is a chance the prior user will be scimmed
-            # in and managed. So if the current user is in state and we run destroy we should run terraform state rm curr_user
-            pass
-
-        for command in commands:
-            tf.raw_cmd(command)
-
 
 def fetch_cluster_ids_from_state(state_json):
     try:
